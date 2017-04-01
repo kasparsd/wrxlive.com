b3dc8129314832f30404f8dfb074e47ad39531d3
Kaspars Dambis <hi@kaspars.net>
2017-04-01 20:19:53 +0300

    Add the update script

---
+++ public/update.php
@@ -0,0 +1,14 @@
+<?php
+
+$throttle = ( time() - filemtime( __DIR__ . '/.throttle' ) < 300 );
+
+if ( empty( $_POST['payload'] ) || $throttle ) {
+	return;
+}
+
+touch( __DIR__ . '/.throttle' );
+
+shell_exec( sprintf(
+	'cd %s && git reset --hard HEAD && git pull',
+	escapeshellarg( __DIR__ )
+) );
